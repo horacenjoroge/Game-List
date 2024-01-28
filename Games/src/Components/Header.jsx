51200e82eb5963e14b195db0a29797557835f1fc
horacenjoroge <125595726+horacenjoroge@users.noreply.github.com>
2024-01-29 00:31:01 +0300

    Styling Header

--- Games/src/Components/Header.jsx
+++ Games/src/Components/Header.jsx
@@ -1,11 +1,17 @@
 import React from 'react'
 import logo from './../assets/Images/logo.png'
+import { HiMagnifyingGlass } from "react-icons/hi2"
+import { FaMoon } from "react-icons/fa";
 function Header() {
   return (
-    <div>
+    <div className='flex items-center p-3'>
       <img src={logo} width={60}height={60} />
+      <div className='flex bg-slate-200 p-2 w-full items-center mx-5 rounded-full'>
+        <HiMagnifyingGlass/>
+        <input type = 'text'placeholder='Search Games'className='bg-transparent outline-none px-2'/>
+      </div>
       <div>
-        <input type = 'text'/>
+              < FaMoon/>
       </div>
     </div>
   )
