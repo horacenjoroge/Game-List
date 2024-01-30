5316e10606b229804774ab848d3167979d21decb
horacenjoroge <125595726+horacenjoroge@users.noreply.github.com>
2024-01-30 07:40:49 +0300

    setActiveIndex

--- Games/src/Components/GenreList.jsx
+++ Games/src/Components/GenreList.jsx
@@ -1,12 +1,16 @@
-import React, { useEffect } from 'react'
+import React, { useEffect ,useState } from 'react'
 import GlobalApi from '../Services/GlobalApi';
 function GenreList() {
+
+    const[genreList,setGenreList] =useState([])
+    const[activeIndex,setActiveIndex] = useState(0)
     useEffect(() => {
         const fetchGenreList = async () => {
             try {
                 // Call the getGenreList function and use await to get the response
                 const resp = await GlobalApi.getGenreList();
                 console.log(resp.data.results);
+                setGenreList(resp.data.results);
             } catch (error) {
                 // Handle errors
                 console.error('Error fetching genre list:', error);
@@ -19,7 +23,30 @@ function GenreList() {
 
     return (
         <div>
-            GenreList
+          <h2 className='text-[30px] font-bold'>Genre</h2>  
+            {genreList.map((item, index) => (
+                <div
+                    onClick={() => setActiveIndex(index)}
+                    className={`flex gap-2 items-center mb-2 cursor-pointer hover:bg-slate-300 p-2 rounded-lg group
+    ${activeIndex === index ? "bg-slate-300" : null}`}
+                >
+                    <img
+                        src={item.image_background}
+                        className={`w-[120px] h-[120px] object-cover rounded-lg group-hover:scale-105 transition-all-ease-out
+      duration-300 ${activeIndex === index ? "scale-105" : null}`}
+                    />
+                    <h3
+                        className={`group-hover:font-bold transition-all-ease-out duration-300
+      ${activeIndex === index ? "scale-105" : null}`}
+                    >
+                        {item.name}
+                    </h3>
+                </div>
+            ))}
+
+       
+          
+          
         </div>
     );
 }
--- Games/src/Pages/Home.jsx
+++ Games/src/Pages/Home.jsx
@@ -3,8 +3,8 @@ import GenreList from '../Components/GenreList'
 
 function Home() {
   return (
-    <div className='grid grid-cols-4' >
-      <div className='bg-red-400 h-full hidden md:block'>
+    <div className='grid grid-cols-4 p-2' >
+      <div className=' hidden md:block'>
         <GenreList/>
       </div>
       <div className='col-span-4 md:col-span-3 bg-blue-400'>Game List</div>
